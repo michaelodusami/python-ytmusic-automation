68f91a5e1e16d7cb0d5c1ecd424a8f58da35b4a0
modusami <128193218+modusami@users.noreply.github.com>
2024-03-27 16:57:40 -0400

    create backend_playlist.py

---
+++ backend_playlist.py
@@ -0,0 +1,8 @@
+from ytmusic_instance import my_ytmusic
+import backend_utility
+
+
+def add_playlist_songs_to_playlist(playlist_id : str, source_playlist_id: str, duplicates = False):
+    my_ytmusic.add_playlist_items(playlistId=playlist_id, source_playlist=source_playlist_id, duplicates=bool)
+
+
