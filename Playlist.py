df07c68d6b23fb65325594aafc15f6519eea68a7
modusami <128193218+modusami@users.noreply.github.com>
2024-03-27 16:58:11 -0400

    Delete (Replace) File Names

--- Playlist.py
+++
@@ -1,8 +0,0 @@
-from ytmusic_instance import my_ytmusic
-import utility
-
-
-def add_playlist_songs_to_playlist(playlist_id : str, source_playlist_id: str, duplicates = False):
-    my_ytmusic.add_playlist_items(playlistId=playlist_id, source_playlist=source_playlist_id, duplicates=bool)
-
-
