e1210fc6235cad3680a55ddcb07d0b1eacf254a6
modusami <128193218+modusami@users.noreply.github.com>
2024-03-27 19:10:39 -0400

    Remove duplicate parameter

--- backend_playlist.py
+++ backend_playlist.py
@@ -2,7 +2,7 @@ from ytmusic_instance import my_ytmusic
 import backend_utility
 
 
-def add_playlist_songs_to_playlist(playlist_id : str, source_playlist_id: str, duplicates = False):
-    my_ytmusic.add_playlist_items(playlistId=playlist_id, source_playlist=source_playlist_id, duplicates=bool)
+def add_playlist_songs_to_playlist(playlist_id : str, source_playlist_id: str):
+    my_ytmusic.add_playlist_items(playlistId=playlist_id, source_playlist=source_playlist_id, duplicates=True)
 
 
