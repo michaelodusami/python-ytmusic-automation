c66747db42f10096bb2dc963762fc5beca0af618
modusami <128193218+modusami@users.noreply.github.com>
2024-03-27 20:12:04 -0400

    Update service_playlist.py

--- service_playlist.py
+++ service_playlist.py
@@ -5,8 +5,7 @@ Description: Controller module responsible for managing user interactions and AP
 @version: March 2024
 """
 
-import backend_utility
-from backend_playlist import add_playlist_songs_to_playlist
+import backend_playlist 
 
 def add_songs_from_existing_playlist_to_other_playlist(destination_playlist_name, source_playlist_name):
     """
@@ -28,11 +27,11 @@ def add_songs_from_existing_playlist_to_other_playlist(destination_playlist_name
     """
     try:
         # Retrieve playlist IDs
-        playlist_to_add_to_id = backend_utility.get_playlist_id(destination_playlist_name)
-        source_playlist_id = backend_utility.get_playlist_id(source_playlist_name)
+        playlist_to_add_to_id = backend_playlist.get_playlist_id(destination_playlist_name)
+        source_playlist_id = backend_playlist.get_playlist_id(source_playlist_name)
         
         # Add songs to the destination playlist
-        add_playlist_songs_to_playlist(playlist_id=playlist_to_add_to_id, source_playlist_id=source_playlist_id)
+        backend_playlist.add_playlist_songs_to_playlist(playlist_id=playlist_to_add_to_id, source_playlist_id=source_playlist_id)
     
     except Exception as e:
         print(e)
