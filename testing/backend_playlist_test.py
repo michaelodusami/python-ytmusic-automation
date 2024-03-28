157106e95d171ee1868b3b820f9bc19c3b4fbcaa
modusami <128193218+modusami@users.noreply.github.com>
2024-03-27 23:34:29 -0400

    Create test file

---
+++ testing/backend_playlist_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class Backend_Playlist_Test(unittest.TestCase):
+    
+
+    
+
+
+
+
+
+    # sample test class
+    def test_add(self):
+        result = 10 + 10
+        self.assertEqual(20, result)
+
+
+unittest.main()
